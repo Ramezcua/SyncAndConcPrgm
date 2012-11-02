472a6321ff99d01987a1e85b58b296ced2e2dca1
Ricco Amezcua <ramezcua7@gmail.com>
2012-11-02 12:28:25 -0500

    Thread code is working

--- 1/golfing.py
+++ 1/golfing.py
@@ -3,13 +3,31 @@ from random import random
 from time import sleep
 import sys
 
-def cart()
+def cart():
+	while True:
+		sleep(random() * 5 )
+		print "I am a cart"
 
-def golfer(my_id)
+def golfer(my_id):
+	while True:
+		sleep(random() * 5)
+		print "I am golfer %d" % my_id
 
 STASH_SIZE 	= 20
 BUCKET_SIZE = 5
 NUM_GOLFERS = 5
 
 if __name__ == '__main__':
+	stash = STASH_SIZE
 
+	gthreads = []
+	for i in range(NUM_GOLFERS):
+		tid = i
+		gthreads.append(Thread(target=golfer, args=[tid]))
+
+	c = Thread(target=cart)
+	c.start()
+
+	for t in gthreads:
+		t.start()
+	
