064718a16c86ec818ea32ced791cfa36cd1451b0
Ricco Amezcua <ramezcua7@gmail.com>
2012-11-02 12:09:45 -0500

    Real Golfing Skeleton

---
+++ 1/golfing.py
@@ -0,0 +1,15 @@
+from threading import Semaphore, Thread
+from random import random
+from time import sleep
+import sys
+
+def cart()
+
+def golfer(my_id)
+
+STASH_SIZE 	= 20
+BUCKET_SIZE = 5
+NUM_GOLFERS = 5
+
+if __name__ == '__main__':
+
